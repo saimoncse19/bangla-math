42f1c6911c869582f6597e5f418c0914ab229cb6
saimon <saimoncse19@gmail.com>
2020-02-21 00:55:07 +0600

    initial commit

---
+++ mathevaluator/benmatheval.py
@@ -0,0 +1,79 @@
+"""
+    Bangla math evaluator calculates the result of any bengali mathematical
+    expression and returns it.
+
+    Author: Saimon
+    Email: saimoncse19@gmail.com
+
+"""
+
+
+class Evaluator:
+
+    def __str__(self):
+        self._digits = ["০", "১", "২", "৩", "৪", "৫", "৬", "৭", "৮", "৯"]
+        self._operators = ["=", "+", "-", "*", "/", "%", "<", ">", "×", "÷"]
+
+    def bengali_to_english(self, expression: str):
+
+        exp_len = len(expression)
+        temp = 0
+
+        for i in expression:
+            if i in self._digits or i in self._operators or i in ["(", ")", "{", "}", "[", "]"]:
+                temp += 1
+
+        if exp_len == temp:
+            validity = True
+        else:
+            validity = False
+
+        if validity:
+            sub = "".join(self._digits) + "".join(self._operators) + "[]{}()."
+            sub_with = "0123456789=+-*/%<>*+" + "()()()."
+
+            trans_table = str.maketrans(sub, sub_with)
+            exp = expression.translate(trans_table)
+
+            try:
+                result = eval(exp)
+
+            except (SyntaxError, TypeError) as e:
+                return f"Invalid Syntax. Try again."
+
+            else:
+                return result
+
+    def bengali_to_bengali(self, expression: str):
+        exp_len = len(expression)
+        temp = 0
+
+        for i in expression:
+            if i in self._digits or i in self._operators or i in ["(", ")", "{", "}", "[", "]", "."]:
+                temp += 1
+
+        if exp_len == temp:
+            validity = True
+        else:
+            validity = False
+
+        if validity:
+            sub = "".join(self._digits) + "".join(self._operators) + "[]{}()."
+            sub_with = "0123456789=+-*/%<>*+" + "()()()."
+
+            trans_table = str.maketrans(sub, sub_with)
+            exp = expression.translate(trans_table)
+
+            try:
+                result = eval(exp)
+
+            except (SyntaxError, TypeError) as e:
+                return f"Invalid Syntax. Try again."
+
+            else:
+                sub = "0123456789."
+                sub_with = "".join(self._digits) + "."
+                trans_table = str.maketrans(sub, sub_with)
+                result_in_bengali = str(result).translate(trans_table)
+                return result_in_bengali
+
